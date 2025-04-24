4ee219c295bce2b7c62020ccbad7a4e613a41d32
LamanAliyeva <laliyeva18053@ada.edu.az>
2025-04-25 02:17:45 +0400

    First Commit
    Created Gradle app for Back-end

---
+++ restaraunt-back/src/test/java/az/edu/ada/restaurant/RestarauntApplicationTests.java
@@ -0,0 +1,13 @@
+package az.edu.ada.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestarauntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
