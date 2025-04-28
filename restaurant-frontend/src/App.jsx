9243799e2f677242d494a591eee08a880a8b019b
Leyla Eynullazada <leynullazada18033@ada.edu.az>
2025-04-28 21:37:08 +0400

    Created Front-End Project
    Vite + React

---
+++ restaurant-frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
+import "./App.css"
+
+function App() {
+  return (
+    <Router>
+    </Router>
+  )
+}
+
+export default App
