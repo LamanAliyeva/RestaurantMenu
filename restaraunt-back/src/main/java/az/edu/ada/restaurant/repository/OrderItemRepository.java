49863e80f9b37d5c12bf5534c48e75eae1608fed
LamanAliyeva <laliyeva18053@ada.edu.az>
2025-05-14 15:29:08 +0400

    Minor Bug Fix

--- restaraunt-back/src/main/java/az/edu/ada/restaurant/repository/OrderItemRepository.java
+++
@@ -1,6 +0,0 @@
-package az.edu.ada.restaurant.repository;
-
-import az.edu.ada.restaurant.model.OrderItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {}
--- restaraunt-back/src/main/java/az/edu/ada/restaurant/service/CustomUserDetailsService.java
+++ restaraunt-back/src/main/java/az/edu/ada/restaurant/service/CustomUserDetailsService.java
@@ -3,9 +3,7 @@ package az.edu.ada.restaurant.service;
 import az.edu.ada.restaurant.model.User;
 import az.edu.ada.restaurant.repository.UserRepository;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.core.userdetails.*;
 import org.springframework.stereotype.Service;
 
 import java.util.stream.Collectors;
